074846864fa2892e0651ce6d0a936848e703783d
Adam Miller <adam.miller@csgi.com>
2021-03-22 14:05:00 -0500

    get data at runtime

--- src/templates/trailerDisplay.tsx
+++ src/templates/trailerDisplay.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
@@ -6,6 +6,16 @@ import SEO from "../components/seo"
 
 export default function TrailerDisplay({ data }) {
   const image = getImage(data.graphCmsModel.coverImage)
+
+  // Client-side Runtime Data Fetching
+  const [blogData, setBlogData] = useState()
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setBlogData(resultData)
+      })
+  }, [])
 
   return (
     <Layout>
@@ -17,6 +27,8 @@ export default function TrailerDisplay({ data }) {
       <div style={{ whiteSpace: "pre-wrap" }}>
         {data.graphCmsModel.marketingText}
       </div>
+      <h3>Runtime Data</h3>
+      <div>{blogData?.title}</div>
     </Layout>
   )
 }
